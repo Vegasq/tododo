44744e2afa49bfef7c17aadde911eb0c2b9aec4b
Nikolay Yakovlev <nyakovlev@mirantis.com>
2014-11-04 17:23:54 +0200

    Important switcher

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir /opt/tododo
+cp tododo.py /opt/tododo/tododo.py
+cp temp-todo-icon.png /opt/tododo/temp-todo-icon.png
+chmod 777 /opt/tododo/temp-todo-icon.png
+cp tododo.desktop /usr/share/applications/tododo.desktop
+
